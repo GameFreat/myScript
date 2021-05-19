ef0886dcc0cb4796dae5eed76ae8aade2579e051
GameFreat <naveenbjacob123@gmail.com>
2021-05-19 22:46:51 +0530

    added aliases

--- install.sh
+++ install.sh
@@ -1,11 +1,31 @@
 clear
 printf '\t\t\t__Welcome to my script___\n'
-# echo '\t __Welcome to my script___'
 echo "This script install NVM ,also Rocket, a bash script to style the bash prompt"
 echo "This script also adds my some personal aliases too."
 echo " "
+echo "Do you want to install Rocket?(y/n)"
+read Rocket
+if [[$Rocket -eq "y"]];then
+    git clone https://github.com/xenomech/Rocket.git
+    cd Rocket
+    echo "Do you want to install the plain version or the emoji version?(p/e)"
+    read varient
+    ./install $varient
+fi
 echo "Do you want to install NVM?(y/n)"
 read nvm
 if [[ $nvm -eq "y" ]];then
-    
+    cat "# ---------------NVM----------" >>~/.bashrc
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+    source ~/.bashrc
+    nvm install 'lts/*'
 fi
+echo "Wanna add aliases like 'home=cd ~' and 'root=cd /'?(y/n)"
+read resp
+if [[ $resp -eq "y" ]];then
+    cat "# Personal Aliases" >>~/.bashrc
+    cat "alias home='cd ~'">>~/.bashrc
+    cat "alias root='cd /'">>~/.bashrc 
+    source
+fi
+echo "Completed!!!!!!!"
