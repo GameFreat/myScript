6013d30e1b70950405379942d1e8f2b91f723ba5
GameFreat <naveenbjacob123@gmail.com>
2021-05-19 22:18:10 +0530

    initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+clear
+printf '\t\t\t__Welcome to my script___\n'
+# echo '\t __Welcome to my script___'
+echo "This script install NVM ,also Rocket, a bash script to style the bash prompt"
+echo "This script also adds my some personal aliases too."
+echo " "
+echo "Do you want to install NVM?(y/n)"
+read nvm
+if [[ $nvm -eq "y" ]];then
+    
+fi
